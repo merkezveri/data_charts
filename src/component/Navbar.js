064fe66d74ec643a5a5c9130152c1dd5e166bac2
Furkan Tolga Yüce <yucefurkantolga@gmail.com>
2020-12-21 01:27:53 +0300

    Deploy

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -4,12 +4,14 @@ import logo from '../figures/logo-white.png';
 
 function Navbar() {
     return (
-        <nav class="navbar navbar-dark bg-dark border-bottom border-2 mb-4">
-            <div className="container justify-content-center">
-                <a class="navbar-brand" href="/"><img src={logo} className="image mb-1" /></a>
-                
-            </div>
-        </nav>
+        <header id="header">
+            <nav class="navbar navbar-dark bg-dark border-bottom border-2 mb-4">
+                <div className="container justify-content-center">
+                    <a class="navbar-brand" href="/"><img src={logo} className="image mb-1" /></a>
+                    
+                </div>
+            </nav>
+        </header>
     )
 }
 
